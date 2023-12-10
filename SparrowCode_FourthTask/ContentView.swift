24e0a3335ca7e5a906cec3716533b10e817e4eec
Edmond <edpodlegaev@yandex.ru>
2023-12-10 05:14:03 +0300

    Add code from previous solution

--- SparrowCode_FourthTask/ContentView.swift
+++ SparrowCode_FourthTask/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var performAnimation = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            if !performAnimation {
+                withAnimation(.interpolatingSpring(stiffness: 170, damping: 15)) {
+                    performAnimation = true
+                } completion: {
+                    performAnimation = false
+                }
+            }
+        } label: {
+            GeometryReader { proxy in
+                
+                let width = proxy.size.width / 2
+                let systemName = "play.fill"
+                
+                HStack(alignment: .center, spacing: 0) {
+                    Image(systemName: systemName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: performAnimation ? width : .zero)
+                        .opacity(performAnimation ? 1 : .zero)
+                    Image(systemName: systemName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width)
+                    Image(systemName: systemName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: performAnimation ? 0.5 : width)
+                        .opacity(performAnimation ? .zero : 1)
+                }
+                .frame(maxHeight: .infinity, alignment: .center)
+            }
         }
-        .padding()
+        .frame(maxWidth: 65)
     }
 }
 
